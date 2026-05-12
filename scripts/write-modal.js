810493eb3a3d637fccc108245520032f0154cac5
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-05-12 14:34:23 -0500

    Removed patch script files.

--- scripts/write-modal.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-const p = 'app/staff/(protected)/imports/[id]/map/_components/EditDemoSessionModal.tsx';
-const src = fs.readFileSync('scripts/write-modal.txt', 'utf8');
-fs.writeFileSync(p, src, 'utf8');
-console.log('done, lines:', src.split('\n').length);
